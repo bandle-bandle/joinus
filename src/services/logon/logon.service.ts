8c754e3f8f76bdf3916f62baa2ecd8e0e71afef5
m-toda <m-toda@unixon.co.jp>
2020-02-21 10:14:04 +0900

    アカウント作成修正

--- src/services/logon/logon.service.ts
+++ src/services/logon/logon.service.ts
@@ -11,10 +11,11 @@ export class LogonService {
   async logon(data,auth){
     try {
       // 権限登録
-      var ref=await firebase.firestore().doc('auth/'+auth.email).set({id:auth.email});
+      var ref=await firebase.firestore().doc('auth/'+auth.email).set({id:data.id});
       await firebase.auth().createUserWithEmailAndPassword(auth.email,auth.password)
         .then((res: any) => console.log(res))
         .catch((error: any) => console.error(error));
+
       // soloアカウント作成
       var ref=await firebase.firestore().doc('solo_account/'+data.email).set(data);
       this.result = {status: "success", "msg": "logon is correct", ref:this.auth_id};
